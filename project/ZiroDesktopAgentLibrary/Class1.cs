959bd7127d641cfd0377d8d255353f4fa2dd6cda
Widwards <thewidwards@gmail.com>
2013-05-08 22:16:15 +0600

    1st update.

---
+++ project/ZiroDesktopAgentLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZiroDesktopAgentLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ project/ZiroServerWcfServiceLibrary/ZiroMainService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+namespace ZiroServerWcfServiceLibrary
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class ZiroMainService : IZiroService
+    {
+
+        private float currentCpuUsage;
+        private float currentFreeMemory;
+
+
+        //дефолтные настройки для клиента
+        public int GetDefaultOptions()
+        {
+            throw new NotImplementedException();
+        }
+
+        //КЛИЕНТ:
+        public void PushCurrentCpuUsage()
+        {
+            throw new NotImplementedException();
+        }
+
+        //КЛИЕНТ:
+        public void PushCurrentFreeMemory()
+        {
+            throw new NotImplementedException();
+        }
+
+        //СЕРВЕР:
+        public float GetCurrentCpuUsage()
+        {
+            throw new NotImplementedException();
+        }
+
+        //СЕРВЕР:
+        public float GetCurrentFreeMemory()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
