da4552c059d93263677834c921e77bbd07589b65
Widwards <thewidwards@gmail.com>
2013-05-23 21:50:43 +0600

    1st varitan GUI for Desktop viewer

--- project/ZiroDesktopApp/MainWindow.xaml.cs
+++ project/ZiroDesktopApp/MainWindow.xaml.cs
@@ -22,19 +22,112 @@ namespace ZiroDesktopApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        Application app = null;
         public MainWindow()
         {
             InitializeComponent();
-            app = new Application();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void FileExit_Click(object sender, RoutedEventArgs e)
         {
-            
-
-            app.Shutdown();
+            this.Close();
         }
-        
+
+        private void FileOpen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZiroServerConnect_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("To main Ziro server connected");
+        }
+
+        private void TestZiroServerConnect_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("To test Ziro server connected");
+        }
+
+        //StatusBar evets
+        private void MouseEnterExitArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "Close application";
+        }
+        private void MouseLeaveAnyArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "Ready";
+        }
+        private void MouseEnterToolsArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "Tools";
+        }
+        private void MouseEnterOpenFileArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "Open file";
+        }
+        private void MouseEnterEditArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "Edit";
+        }
+        private void MouseEnterHelpArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "Help";
+        }
+        private void MouseEnterTestZiroServerConnectionArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "Test Ziro server";
+        }
+        private void MouseEnterZiroServerConnectionArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "Main Ziro server";
+        }
+        private void MouseEnterConnectionToDbArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "List of connections to database";
+        }
+        private void MouseEnterDbArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "Data Base options";
+        }
+        private void MouseEnterFileArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "File";
+        }
+        private void MouseEnterWidwardsZiroServerConnectionArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "Widwards.cloudapp.net Ziro server";
+        }
+        private void MousEnterListOfNodesArea(object sender, MouseEventArgs e)
+        {
+            statusBarText.Text = "List of connecting nodes";
+        }
+
+        private void ResizControl_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        //dragable
+        private void MousLeftButtonDown_click(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void WidwardsZiroServerConnect_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("To widwards.cloudapp.net database connected");
+        }
+
+        private void ListBoxChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+
+
     }
 }
